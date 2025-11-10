3c84a8abb5dbcfa8dd33a680390a2571e5323832
Angel Fernández <65675298+lfer9980@users.noreply.github.com>
2025-11-10 11:34:21 -0600

    [HOTFIX] import error path

--- backend/app/src/analysis/infrastructure/celery/report.py
+++ backend/app/src/analysis/infrastructure/celery/report.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from app.infrastructure.celery.celery_app import celery
+from app.infrastructure.celery.celery_app import celery, get_or_create_event_loop
 from app.infrastructure.database import DatabaseSession
 from app.src.analysis.infrastructure.celery.subtasks import (
     save_mae_family_data,
@@ -11,9 +11,8 @@ from app.src.analysis.infrastructure.celery.subtasks import (
     save_topsis_data,
     update_job_status,
 )
-from app.src.analysis.infrastructure.repository.report_repository import (
-    ReportRepositorySQL,
-)
+from app.src.analysis.infrastructure.repository.report_repository import ReportRepositorySQL
+
 from celery import shared_task
 
 ReportsType = tuple[list[dict], list[dict], str, list[dict]]
@@ -79,8 +78,6 @@ def create_report_task(self, new_data: ReportsType, job_id: str):
                 session=session,
                 job_id=job_id,
             )
-    
-    from app.infrastructure.celery_app import get_or_create_event_loop
-    
+        
     loop = get_or_create_event_loop()
     return loop.run_until_complete(run_task())
